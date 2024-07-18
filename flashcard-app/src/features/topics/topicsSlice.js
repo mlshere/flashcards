803e98cb91e307c5ed186f265510c5de7b856a5e
mlshere <mlsherezade@gmail.com>
2024-07-18 14:13:35 +0200

    Create topic slice addTopic

---
+++ flashcard-app/src/features/topics/topicsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    topics: {}
+}
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState,
+    reducers: {
+        addTopic: (state, action) => {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {
+                id, 
+                name, 
+                icon,
+                quizIds: []
+            };
+        }
+    }
+});
+
+export const { addTopic } = topicsSlice.actions;
+export const selectTopics = state => state.topics.topics;
+export default topicsSllice.reducer;
