55df596082461769a8c46143b02535f7ae4672a7
mlshere <mlsherezade@gmail.com>
2024-07-19 10:44:49 +0200

    Fix tipos and routing

--- flashcard-app/src/features/quizzes/NewQuizForm.js
+++ flashcard-app/src/features/quizzes/NewQuizForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addQuiz } from './quizzesSlice';
-import { selectTopics } from '../topics/topicsSlice';
+import { addQuiz, } from './quizzesSlice';
+import { selectTopics, addQuizIdToTopic } from '../topics/topicsSlice';
+import { selectCardById, addCard } from '../cards/cardsSlice'
 
 export const NewQuizForm = ({ history }) => {
     const [name, setName] = useState('');
@@ -12,6 +13,16 @@ export const NewQuizForm = ({ history }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const id = uuidv4();
+        const cardIds = [];
+
+        // Iterate over local state containing cards data
+        selectCardById.forEach(card => {
+            const cardId = uuidv4();
+            dispatch(addCard({ id: cardId, front: card.front, back: card.back}));
+            cardIds.push(cardId);
+        })
+
         dispatch(addQuiz({ id, name, topicId, cardIds }));
         dispatch(addQuizIdToTopic({ topicId, quizId: id }));
         history.push('/quizzes');
