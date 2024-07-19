aa19efe287c641999209da14cee1f799bc713770
mlshere <mlsherezade@gmail.com>
2024-07-19 16:04:07 +0200

    Update imports

--- flashcard-app/src/features/topics/Topics.js
+++ flashcard-app/src/features/topics/Topics.js
@@ -6,12 +6,29 @@ export const Topics = () => {
     const topics = useSelector(selectTopics);
 
     return (
-        <div>
-            {Object.values(topics).maps(topic => (
-                <div key={topic.id}>
-                    <h2>{topic.name}</h2>
-                </div>
+        <section className="center">
+          <h1>Topics</h1>
+          <ul className="topics-list">
+            {Object.values(topics).map((topic) => (
+              <li className="topic" key={topic.id}>
+              <Link to={ROUTES.topicRoute(topic.id)} className="topic-link">
+               <div className="topic-container">
+                 <img src={topic.icon} alt="" />
+                 <div className="text-content">
+                   <h2>{topic.name}</h2>
+                   <p>{topic.quizIds.length} Quizzes</p>
+                 </div>
+               </div>
+             </Link>
+              </li>
             ))}
-        </div>
-    )
-}
+          </ul>
+          <Link
+            to={ROUTES.newTopicRoute()}
+            className="button create-new-topic-button"
+          >
+            Create New Topic
+          </Link>
+        </section>
+      );
+    }
