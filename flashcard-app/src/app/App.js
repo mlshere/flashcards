cf8f51dd84e99223aa30be8ca6ce6174cfe050ca
mlshere <mlsherezade@gmail.com>
2024-07-19 12:08:35 +0200

    Update / route

---
+++ flashcard-app/src/app/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NewTopicForm } from '../features/topics/NewTopicForm';
+import { Topics } from '../features/topics/Topics';
+import { Quiz } from '../features/quizzes/Quiz';
+import { NewQuizForm } from '../features/quizzes/NewQuizForm';
+import { AppLayOut } from './AppLayOut';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayOut />} />
+        <Route path="/topics/new" component={<NewTopicForm />} />
+        <Route path="/topics/:topicId" component={<Topics />} />
+        <Route path="/topics" component={<Topics />} />
+        <Route path="/quizzes/new" component={<NewQuizForm />} />
+        <Route path="/quizzes/:quizId" component={<Quiz />} />
+        <Route path="/quizzes" component={<Quiz />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
