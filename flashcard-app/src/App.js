85d54cb5e63a347f8c37cc35156e328b571f60a7
mlshere <mlsherezade@gmail.com>
2024-07-19 10:42:05 +0200

    Fix Routing

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { NewTopicForm } from './features/topics/NewTopicForm';
+import { Topics } from './features/topics/Topics';
+import { Quiz } from './features/quizzes/Quiz';
+import { NewQuizForm } from './features/quizzes/NewQuizForm';
 
 
 function App() {
   return (
-    
+    <Router>
+        <Route path="/topics/new" component={NewTopicForm} />
+        <Route path="/topics/:topicId" component={Topics} />
+        <Route path="/topics" component={Topics} />
+        <Route path="/quizzes/new" component={NewQuizForm} />
+        <Route path="/quizzes/:quizId" component={Quiz} />
+        <Route path="/quizzes" component={Quiz} />
+    </Router>
   );
 }
 
